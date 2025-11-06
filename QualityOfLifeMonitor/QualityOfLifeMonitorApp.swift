38dce21f12e0b20de0a55ac8a0bf2b0802d79913
omar <omarrahman96@gmail.com>
2025-11-06 07:30:00 +0000

    Initial Commit

---
+++ QualityOfLifeMonitor/QualityOfLifeMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  QualityOfLifeMonitorApp.swift
+//  QualityOfLifeMonitor
+//
+//  Created by Omar Rahman on 06/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct QualityOfLifeMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
